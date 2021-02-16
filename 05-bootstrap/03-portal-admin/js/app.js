11cf94b0b945f7c95b0811d284d2dc1d0910147f
jcpinheiro <jcpinheiro@ifma.edu.br>
2021-02-16 19:18:43 -0300

    Material bootstrap

---
+++ 05-bootstrap/03-portal-admin/js/app.js
@@ -0,0 +1,39 @@
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
+
+
+fetch('data.json')
+  .then(data => data.json())
+  .then(respuesta => {
+      new Morris.Line({
+        // ID of the element in which to draw the chart.
+        element: 'ventas',
+        // Chart data records -- each entry in this array corresponds to a point on
+        // the chart.
+        data: respuesta,
+        // The name of the data record attribute that contains x-values.
+        xkey: 'year',
+        // A list of names of data record attributes that contain y-values.
+        ykeys: ['value'],
+        // Labels for the ykeys -- will be displayed when you hover over the
+        // chart.
+        labels: ['Value']
+      });
+  });
+
+
